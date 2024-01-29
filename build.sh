c00e11d71f29f6cd8bbd6b594d8bbed18790ab5d
Maxime Marcelin <maisonmarc7@gmail.com>
2024-01-29 11:56:58 +0100

    Added a small build script.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Checking if the build directory must be re-created.
+if test -d ./build
+then 
+    rm -r build
+fi
+
+# Creating the build folder.
+mkdir build
+cd build
+
+# Building project
+cmake ..
+make
